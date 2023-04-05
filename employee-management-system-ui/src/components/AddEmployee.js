058bea5276a4a09b2df8c19d8e8c3a58de0ab71c
keer2345 <keer2345@gmail.com>
2023-04-05 08:23:47 +0800

    react AddEmployee.js

--- employee-management-system-ui/src/components/AddEmployee.js
+++ employee-management-system-ui/src/components/AddEmployee.js
@@ -5,11 +5,41 @@ const AddEmployee = () => {
         <div className='font-thin text-2xl'>
           <h1>Add New Employee</h1>
         </div>
-        <div className='items-center justify-center h-14 w-full'>
+        <div className='items-center justify-center h-14 w-full my-4'>
           <label className='block text-gray-600 text-sm font-normal'>
             First Name
           </label>
-          <input type='text' className='h-10 w-96 border mt-2'></input>
+          <input
+            type='text'
+            className='h-10 w-96 border mt-2 px-2 py-2'></input>
+        </div>
+        <div className='items-center justify-center h-14 w-full my-4'>
+          <label className='block text-gray-600 text-sm font-normal'>
+            Last Name
+          </label>
+          <input
+            type='text'
+            className='h-10 w-96 border mt-2 px-2 py-2'></input>
+        </div>
+        <div className='items-center justify-center h-14 w-full my-4'>
+          <label className='block text-gray-600 text-sm font-normal'>
+            Email
+          </label>
+          <input
+            type='email'
+            className='h-10 w-96 border mt-2 px-2 py-2'></input>
+        </div>
+        <div className='items-center justify-center h-14 w-full my-2 space-x-4 pt-4'>
+          <button
+            className='rounded text-white font-semibold
+           bg-green-400 hover:bg-green-700 py-2 px-6'>
+            Save
+          </button>
+          <button
+            className='rounded text-white font-semibold
+           bg-red-400 hover:bg-red-700 py-2 px-6'>
+            Clear
+          </button>
         </div>
       </div>
     </div>
